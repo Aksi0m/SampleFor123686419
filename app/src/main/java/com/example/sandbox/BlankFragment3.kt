47f3fd743238cfc147b4eae31235390c2a9cf264
Renato Turic <renatoturic@Renatos-MacBook-Pro-3.local>
2019-04-04 00:57:52 +0200

    Add demo for navigation action case with multiple fragments of the same class

--- app/src/main/java/com/example/sandbox/BlankFragment3.kt
+++ app/src/main/java/com/example/sandbox/BlankFragment3.kt
@@ -7,19 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class BlankFragment2 : Fragment() {
+class BlankFragment3 : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank_fragment2, container, false)
+        return inflater.inflate(R.layout.fragment_blank_fragment3, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        BlankFragment2Directions.actionBlankFragmentToBlankFragment4()
-        BlankFragment1Directions.
-    }
+
 }
